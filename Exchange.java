103ed64da312ffd20a4d5e12d7a9ba07521dfda2
September J <2286954217@qq.com>
2019-10-28 22:15:08 +0800

    交换两个数字

---
+++ Exchange.java
@@ -0,0 +1,9 @@
+public class Exchange {
+    public int num1;
+    public int num2;
+    public void exchange(){
+        int tmp = num1;
+        int num1 = num2;
+        int num2 = tmp;
+    }
+}
